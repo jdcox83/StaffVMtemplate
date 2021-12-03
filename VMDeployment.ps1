7aaac36a12c2f85d06ad199084082795fe9906dd
jdcox <jdcox@illinois.edu>
2021-12-03 11:31:13 -0600

    Updated Paramater URI

--- VMDeployment.ps1
+++ VMDeployment.ps1
@@ -5,7 +5,7 @@ This will create a new VM for updating the Gies Staff Golden Image
 
 #Login to Azure
 
-Connect-AzAccount -SubscriptionName urbana-vcl-citrix
+#Connect-AzAccount -SubscriptionName urbana-vcl-citrix
 
 
 # Deploy New VM to Azure
@@ -15,4 +15,4 @@ New-AzResourceGroupDeployment `
     -Name Gies-Staff-VM-Update `
     -ResourceGroupName Common-Image-Gold `
     -TemplateUri "https://github.com/jdcox83/StaffVMtemplate/blob/main/StaffVM-template.json" `
-    -TemplateParameterUri "https://github.com/jdcox83/StaffVMtemplate/blob/main/StaffVM-template.json"
+    -TemplateParameterUri "https://github.com/jdcox83/StaffVMtemplate/blob/main/StaffVM-parameters.json"
