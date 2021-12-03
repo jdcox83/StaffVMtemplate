d580c25d906880240e4092eb7f38978ad6fc2e23
jdcox <jdcox@illinois.edu>
2021-12-03 10:59:53 -0600

    Created Deployment PWSH file

---
+++ VMDeployment.ps1
@@ -0,0 +1,18 @@
+<#  
+This will create a new VM for updating the Gies Staff Golden Image
+#>
+
+
+#Login to Azure
+
+Connect-AzAccount -SubscriptionName urbana-vcl-citrix -Identity 
+
+
+# Deploy New VM to Azure
+
+
+New-AzResourceGroupDeployment `
+    -Name Gies-Staff-VM-Update `
+    -ResourceGroupName Common-Image-Gold `
+    -TemplateUri "https://github.com/jdcox83/StaffVMtemplate/blob/main/StaffVM-template.json" `
+    -TemplateParameterUri "https://github.com/jdcox83/StaffVMtemplate/blob/main/StaffVM-template.json"
