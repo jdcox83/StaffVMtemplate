53e6b3d3d50dd42cc4e7b194d96de70cb4987863
John Cox <jdcox@illinois.edu>
2022-02-07 11:45:51 -0600

    replaced incorrect git link

--- VMDeployment.ps1
+++ VMDeployment.ps1
@@ -5,7 +5,7 @@ This will create a new VM for updating the Gies Staff Golden Image
 
 #Login to Azure
 
-#Connect-AzAccount -SubscriptionName urbana-vcl-citrix
+Connect-AzAccount -SubscriptionName urbana-vcl-citrix
 
 
 # Deploy New VM to Azure
@@ -14,5 +14,5 @@ This will create a new VM for updating the Gies Staff Golden Image
 New-AzResourceGroupDeployment `
     -Name Gies-Staff-VM-Update `
     -ResourceGroupName Common-Image-Gold `
-    -TemplateUri "https://raw.githubusercontent.com/jdcox83/StaffVMtemplate/main/StaffVM-template.json" `
+    -TemplateUri "https://raw.githubusercontent.com/jdcox83/StaffVMtemplate/main/StaffVM-Updates-template.json" `
     -TemplateParameterUri "https://raw.githubusercontent.com/jdcox83/StaffVMtemplate/main/StaffVM-Updates-parameters.json"
